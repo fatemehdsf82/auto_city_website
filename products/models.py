ccb2985219407f90aa1a798bae5b10e075cf0c25
fatemehdsf82 <fdastyafteh@gmail.com>
2023-11-30 20:23:57 +0330

    some_changes_in_product_model

--- products/models.py
+++ products/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from datetime import datetime
 
 
-
 class Product(models.Model):
     CATEGORY_CHOICES = [
         ("ghataat_badaneh", "ghataat_badaneh"),
@@ -19,7 +18,6 @@ class Product(models.Model):
         ("looleh", "looleh"),
         ("tasmeh", "tasmeh"),
         ("shelang", "shelang"),
-
     ]
 
     BRAND_CHOICES = [
@@ -32,19 +30,31 @@ class Product(models.Model):
         ("jaham_lent", "jaham_lent"),
         ("behran", "behran"),
         ("isaco", "isaco"),
-
-
     ]
     product_id = models.AutoField(primary_key=True)
-    product_name = models.CharField(max_length=100,name="name")
-    product_cost = models.IntegerField(name="cost")
-    product_category = models.CharField(max_length=40,choices=CATEGORY_CHOICES,name="category")
-    product_brand = models.CharField(max_length=40,choices=BRAND_CHOICES,default='blank',name="brand")
-    product_stock = models.IntegerField(default=0,name="stock")
-    product_created_at = models.DateTimeField(datetime.now,name="created_at")
+    product_name = models.CharField(max_length=100, name="name")
+
+    #! description added
+    product_description = models.TextField(null=True)
+    product_cost = models.PositiveIntegerField(name="cost")
+    product_category = models.CharField(
+        max_length=40, choices=CATEGORY_CHOICES, name="category"
+    )
+    product_brand = models.CharField(
+        max_length=40, choices=BRAND_CHOICES, default="blank", name="brand"
+    )
+    product_stock = models.IntegerField(default=0, name="stock")
+    #! status added
+    product_status = models.BooleanField(default=True)
+
+    # previous datetime
+    product_created_at = models.DateTimeField(datetime.now, name="created_at")
     product_updated_at = models.DateTimeField(auto_now=True, null=True)
     # image = models.ImageField(upload_to='product_images/bolboring.jpg',width_field=600,height_field=600, default='product_images/bolboring.jpg')
-    
+
+    # ? new datetime
+    # datetime_created = models.DateTimeField(auto_now_add=True)
+    # datetime_modified = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.name
-    
