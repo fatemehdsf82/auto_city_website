bd0e9cb8abdb1657ad2f809ae37d83ccb9de1a5e
fatemehdsf82 <fdastyafteh@gmail.com>
2023-12-08 11:12:09 +0330

    config_url_edited

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from cart.views import cart_detail_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("pagess.urls")),
+    path("", include("pages.urls")),
     # path('accounts/',include('django.contrib.auth.urls')),
     # path('accounts/', include('accounts.urls')),
     path("accounts/", include("allauth.urls")),
